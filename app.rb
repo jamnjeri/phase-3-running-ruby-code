2d82af475478e6d3c94b25a2a23b7dd876f7584f
Jamila <jamnjeri@gmail.com>
2023-02-06 09:46:54 +0300

    Completed Assignment

--- app.rb
+++ app.rb
@@ -4,9 +4,14 @@
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+# print "Hello World!"
+# print "Hello World!"
+# print "Hello World!"
+
+puts "Hello World!"
+print "Pass this test, please."
+# puts [1,2,3].inspect
+p [1,2,3]
 
 # def add
     
@@ -14,10 +19,10 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+# puts add(1,3) + add(2,2)
